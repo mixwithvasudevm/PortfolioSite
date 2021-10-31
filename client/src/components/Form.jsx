4ad7388c92bbcab89b99704962f53787ed795e40
KARTIKE KISHORE <kartikekishore@gmail.com>
2021-10-31 15:35:03 +0530

    add form files

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,59 @@
+import React, { Component ,useMemo,useState} from "react";
+import { Button, Container, Row, Col } from "reactstrap";
+import {createEditor} from 'slate';
+import {Slate, Editable, withReact} from 'slate-react';
+
+const BlogForm =()=>{
+    const editor=useMemo(() => withReact(createEditor()), [])
+    // Add the initial value when setting up our state.
+    const [value, setValue] = useState([
+    {
+         type: 'paragraph',
+         children: [{ text: 'A line of text in a paragraph.' }],
+    },
+    
+    ])
+    return(
+        <Slate
+            editor={editor}
+            value={value}
+            onChange={newValue => setValue (newValue) }
+        >
+             <Editable />
+        </Slate>
+        
+    )
+}
+
+class Form extends Component {
+  render() {
+      
+    return (
+      <div className="ShowBlogs mt-5 mx-lg-4 mb-5">
+        <Container className="ml-5 mr-5 ">
+          <Row className="mb-5">
+            <Col className="mt-5 d-flex align-items-center justify-content-center h1">
+              Add New Blog
+            </Col>
+          </Row>  
+
+<Row>
+  {BlogForm} 
+</Row>
+
+
+         <Row>
+         <Col className="d-flex align-items-center justify-content-center ">
+                <a href="/blogs/add">
+                <Button color="success" >Add Blog</Button>{' '}
+                </a>
+          </Col>
+          </Row>
+        </Container>
+        
+      </div>
+    );
+  }
+}
+
+export default Form;
--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Blogs } from "./Blogs";
+export { default as Add } from "./Add";
 
